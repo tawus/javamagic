c1bc6141ec96059a20e2d05e7ee829831f82d891
Taha Hafeez <tawushafeez@gmail.com>
2011-06-28 05:23:01 +0530

    Closes #1

--- zone-refresh/src/main/resources/com/googlecode/tawus/refresh/mixins/zone-refresh.js
+++ zone-refresh/src/main/resources/com/googlecode/tawus/refresh/mixins/zone-refresh.js
@@ -1,33 +1,46 @@
-if(!Tapestry.ZoneRefresh)
+if (!Tapestry.ZoneRefresh)
 {
-   Tapestry.ZoneRefresh = {};   
+   Tapestry.ZoneRefresh = {};
 }
 
-Tapestry.Initializer.zoneRefresh = function(params)
-{
-   if(Tapestry.ZoneRefresh[params.id])
-   {
-      //Timer already in use
-      return;
-   }
-     
-   var element = $(params.id);
-   $T(element).zoneId = params.id;
-   
-   var keepUpdatingZone = function(e)
-   {
-      var zoneManager = Tapestry.findZoneManager(element);
-      if(zoneManager == null)
+Tapestry.Initializer.zoneRefresh =
+      function(params)
       {
-         e.stop();
-         Tapestry.ZoneRefresh[params.id] = null;
-         return;
-      }
-      
-      zoneManager.updateFromURL(params.URL);
-      
-   };
-   
-   Tapestry.ZoneRefresh[params.id] = new PeriodicalExecuter(keepUpdatingZone, params.period);
-}
+         if (Tapestry.ZoneRefresh[params.id])
+         {
+            // Timer already in use
+            return;
+         }
 
+         var element = $(params.id);
+         $T(element).zoneId = params.id;
+
+         var keepUpdatingZone = function(e)
+         {
+            var zoneManager = Tapestry.findZoneManager(element);
+            if (zoneManager == null)
+            {
+               e.stop();
+               Tapestry.ZoneRefresh[params.id] = null;
+               return;
+            }
+
+            zoneManager.updateFromURL(params.URL);
+
+         };
+
+         Tapestry.ZoneRefresh[params.id] =
+               new PeriodicalExecuter(keepUpdatingZone, params.period);
+      }
+
+Event.observe(window, "beforeunload", function()
+{
+   if (Tapestry.ZoneRefresh)
+   {
+      for ( var propertyName in Tapestry.ZoneRefresh)
+      {
+         var property = Tapestry.ZoneRefresh[propertyName];
+         property.stop();
+      }
+   }
+});
